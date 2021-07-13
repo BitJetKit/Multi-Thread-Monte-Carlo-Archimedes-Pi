6a14da4f22ebae0bde7e23ed5140422b69e365c3
Jonathan Kelly <12926868+BitJetKit@users.noreply.github.com>
2021-07-13 08:43:24 -0700

    Update end-user experience.
    Egypt's 1st Pi record resulted to 3.16: their 2nd record is ~3.14.

--- src/Test.java
+++ src/Test.java
@@ -24,7 +24,7 @@ public class Test {
         System.out.println("Here is the test result.");
         System.out.println("Available processors: " + Runtime.getRuntime().availableProcessors());
         System.out.println("Pi Simulation: " + piSimulationRes);
-        System.out.println("Egypt's original Pi record: " + piEgypt);
+        System.out.println("An Egyptian Pi record: " + piEgypt);
         System.out.println("Error difference: " + (piSimulationRes - piEgypt));
         System.out.println("Error rate: " + (piSimulationRes - piEgypt) / piEgypt * 100 + "%");
         System.out.println("Adjusted Pi result with Archimedes' method: " + pi);
